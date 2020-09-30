1e0a0ce6ce6c7e692d1a2af205fd89531ef93203
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-09-30 07:41:32 +0530

    add `faster` feature; use `itoa` only in that

--- markup/src/lib.rs
+++ markup/src/lib.rs
@@ -79,7 +79,16 @@ macro_rules! display {
 
 display! {
     [bool char f32 f64] => |self_, w| write!(w, "{}", self_),
+}
+
+#[cfg(feature = "faster")]
+display! {
     [u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize] => |self_, w| itoa::fmt(w, *self_),
+}
+
+#[cfg(not(feature = "faster"))]
+display! {
+    [u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize] => |self_, w| write!(w, "{}", self_),
 }
 
 #[inline]
