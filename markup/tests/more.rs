28c8c589c56512687673a9c5782d1ba1ec2a3df2
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-10-01 18:05:17 +0530

    add define! test to /markup/tests/more.rs + refactor asserts

--- markup/tests/more.rs
+++ markup/tests/more.rs
@@ -1,3 +1,13 @@
+markup::define! {
+    Define<'a>(data: &'a [usize]) {
+        foo {
+            @for d in *data {
+                bar { "Hello" @d "World" }
+            }
+        }
+    }
+}
+
 fn dynamic(data: &[usize]) -> impl std::fmt::Display + '_ {
     markup::dynamic! {
         foo {
@@ -33,11 +43,17 @@ fn to_writer(data: &[usize], writer: &mut impl std::fmt::Write) -> std::fmt::Res
 fn t() {
     let data = (1..1000).into_iter().collect::<Vec<_>>();
 
-    assert_eq!(dynamic(&data).to_string().len(), 23879);
+    let define = Define { data: &data }.to_string();
+    let dynamic = dynamic(&data).to_string();
+    let to_string = to_string(&data);
+    let to_writer = {
+        let mut string = String::new();
+        to_writer(&data, &mut string).unwrap();
+        string
+    };
 
-    assert_eq!(to_string(&data).len(), 23879);
-
-    let mut string = String::new();
-    to_writer(&data, &mut string).unwrap();
-    assert_eq!(string.len(), 23879);
+    assert_eq!(define.len(), 23879);
+    assert_eq!(define, dynamic);
+    assert_eq!(define, to_string);
+    assert_eq!(define, to_writer);
 }
