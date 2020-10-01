10ee5c0376b297783f702ad4c2eb93822344cd9d
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-10-01 15:12:14 +0530

    better names

--- markup-proc-macro/src/lib.rs
+++ markup-proc-macro/src/lib.rs
@@ -14,18 +14,18 @@ pub fn define(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
 #[proc_macro]
 pub fn dynamic(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let new = syn::parse_macro_input!(tokens as ast::Dynamic);
-    quote::quote!( #new ).into()
+    let dynamic = syn::parse_macro_input!(tokens as ast::Dynamic);
+    quote::quote!( #dynamic ).into()
 }
 
 #[proc_macro]
 pub fn to_string(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let template = syn::parse_macro_input!(tokens as ast::ToString);
-    quote::quote!( #template ).into()
+    let to_string = syn::parse_macro_input!(tokens as ast::ToString);
+    quote::quote!( #to_string ).into()
 }
 
 #[proc_macro]
 pub fn to_writer(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let template = syn::parse_macro_input!(tokens as ast::ToWriter);
-    quote::quote!( #template ).into()
+    let to_writer = syn::parse_macro_input!(tokens as ast::ToWriter);
+    quote::quote!( #to_writer ).into()
 }
