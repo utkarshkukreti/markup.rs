82a608b615e5b087f1227d4eb869b41e6d28aff6
René Kijewski <rene.kijewski@fu-berlin.de>
2023-10-02 14:45:30 +0200

    Fix tests for rustc 1.56

--- ui-tests/src/lib.rs
+++ ui-tests/src/lib.rs
@@ -1,5 +1,12 @@
 #[test]
 fn ui() {
+    let version = rustc_version::version().unwrap();
+    assert_eq!(version.major, 1);
+
     let fail = trybuild::TestCases::new();
-    fail.compile_fail("fail/*.rs");
+    if version.minor <= 56 {
+        fail.compile_fail("fail-1.56/*.rs");
+    } else {
+        fail.compile_fail("fail-1.72/*.rs");
+    }
 }
