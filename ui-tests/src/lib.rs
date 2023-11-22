f4744164c90aab3c65a4cea418a845699c4ac824
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2023-11-23 00:09:12 +0530

    bump minimum rust version to 1.63.0

--- ui-tests/src/lib.rs
+++ ui-tests/src/lib.rs
@@ -4,8 +4,8 @@ fn ui() {
     assert_eq!(version.major, 1);
 
     let fail = trybuild::TestCases::new();
-    if version.minor <= 56 {
-        fail.compile_fail("fail-1.56/*.rs");
+    if version.minor <= 63 {
+        fail.compile_fail("fail-1.63/*.rs");
     } else {
         fail.compile_fail("fail-1.72/*.rs");
     }
